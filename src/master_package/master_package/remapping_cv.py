69beeec316e33b1dca610e203fa8547cc348ea01
shreyanshsuhane <104053965+shreyanshsuhane@users.noreply.github.com>
2024-05-21 23:50:08 +0800

    Update remapping_cv.py

--- src/master_package/master_package/remapping_cv.py
+++ src/master_package/master_package/remapping_cv.py
@@ -1,39 +1,43 @@
-
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Imu
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import TwistWithCovarianceStamped
 
-class ImuRemapNode(Node):
+class CmdVelRemapNode(Node):
     def __init__(self):
-        super().__init__('imu_remap_node')
+        super().__init__('cmd_vel_remap_node')
         self.subscription = self.create_subscription(
-            Imu,
-            '/imu/data_raw',
-            self.imu_callback,
+            Twist,
+            '/cmd_vel',
+            self.cmd_vel_callback,
             10)
-        self.publisher = self.create_publisher(Imu, '/imu_remapped', 10)
-        # self.get_logger().info('IMU Remap Node has been started.')
+        self.publisher = self.create_publisher(TwistWithCovarianceStamped, '/cmd_vel_remapped', 10)
+        #self.get_logger().info('CmdVel Remap Node has been started.')
 
-    def imu_callback(self, msg):
-        # adjusted_msg = Imu()
-        # adjusted_msg.header = msg.header
-        # adjusted_msg.orientation = msg.orientation
-        # adjusted_msg.angular_velocity = msg.angular_velocity
-        # adjusted_msg.linear_acceleration = msg.linear_acceleration
-
-        # Negate the yaw velocity
-        msg.angular_velocity.z = -1*msg.angular_velocity.z
-
-        # Publish the modified message
-        self.publisher.publish(msg)
-        # self.get_logger().info('Published remapped IMU data.')
+    def cmd_vel_callback(self, msg):
+        # Create a new TwistWithCovarianceStamped message
+        twist_cov_msg = TwistWithCovarianceStamped()
+        
+        # Set the header
+        twist_cov_msg.header.stamp = self.get_clock().now().to_msg()
+        twist_cov_msg.header.frame_id = 'base_link'
+        
+        # Copy Twist data
+        twist_cov_msg.twist.twist = msg
+        
+        # Optionally, you can set the covariance matrix if needed
+        # twist_cov_msg.twist.covariance = [0]*36
+        
+        # Publish the new message
+        self.publisher.publish(twist_cov_msg)
+        #self.get_logger().info('Published remapped cmd_vel data.')
 
 def main(args=None):
     rclpy.init(args=args)
-    imu_fix_node = ImuRemapNode()
-    rclpy.spin(imu_fix_node)
+    node = CmdVelRemapNode()
+    rclpy.spin(node)
 
-    imu_fix_node.destroy_node()
+    node.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
